e0f84000de81b2632658d5453b9e99930654e971
Feynom <53947611+Feynom@users.noreply.github.com>
2021-03-10 09:47:39 +0200

    Add files via upload

---
+++ Lab2.1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "FloatRange.h"
+
+int main()
+{  
+    double number;
+    FloatRange a;
+    cin >> a;
+
+    cout << "Enter a number to check for entry in the specified range: "; cin >> number;
+    a.rangeCheckResult(a.rangeCheck(a, number));
+
+    return 0;
+}
---
+++ UnitTest2.1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab2.1/Lab2.1/FloatRange.h"
+#include "../Lab2.1/Lab2.1/FloatRange.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest21
+{
+	TEST_CLASS(UnitTest21)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			FloatRange TEST(2, 10);
+			Assert::AreEqual(TEST.rangeCheck(TEST, 2.111), true);
+			Assert::AreEqual(TEST.rangeCheck(TEST, 1.999), false);
+			Assert::AreEqual(TEST.rangeCheck(TEST, 9.99), true);
+			Assert::AreEqual(TEST.rangeCheck(TEST, 10.0001), false);
+		}
+	};
+}
